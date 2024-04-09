724ed9c0063f3a5404c81c241c2f148d279e2671
Alexander Popov <alexander.popov233@gmail.com>
2024-04-09 02:13:52 +0200

    content + images

--- src/lib/controllers/data.ts
+++ src/lib/controllers/data.ts
@@ -1,4 +1,4 @@
 export function fillData() {
 	const languages = ["js/html/css", "nodejs", "dart", "c/c++", "python", "lua", "C#", "Rust", "typescript", "SQL", "Cypher"];
-	const technologies = ["React, React-Native, Bootstrap, React-Bootstrap, GraphQL, Neo4j, Express, .Net, ThreeJS, WinForms, Flutter, RazorPages, MySQL, SQLite"];
+	const technologies = ["React, React-Native, Bootstrap, React-Bootstrap, GraphQL, Neo4j, Express, .Net, ThreeJS, WinForms, Flutter, RazorPages, MySQL"];
 }
